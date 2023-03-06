ea9ea21494bf33b75d5218d5c3211a9202772a46
Clementain <clentecarmona@gmail.com>
2023-03-05 18:37:22 -0600

    Ahora se muestra en la pantalla de inicio el status resultante

--- app/src/main/java/com/example/contentproviderdivisas/MainActivity.kt
+++ app/src/main/java/com/example/contentproviderdivisas/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.contentproviderdivisas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.contentproviderdivisas.Overview.OverviewViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var overviewViewModel: OverviewViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtener una instancia del ViewModel
+        overviewViewModel = ViewModelProvider(this).get(OverviewViewModel::class.java)
+
+        // Observar la variable "status" en el ViewModel y actualizar el TextView en consecuencia
+        overviewViewModel.status.observe(this, { status ->
+            findViewById<TextView>(R.id.status_text_view).text = status
+        })
     }
 }
